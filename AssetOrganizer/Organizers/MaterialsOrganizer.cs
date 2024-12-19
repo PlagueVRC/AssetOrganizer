43213ee0b206efec7d38e6ae869c323b99f7f7fa
Kanna <36628963+MistressPlague@users.noreply.github.com>
2024-12-19 11:18:19 +0000

    Materials organizer will now retrieve material swaps, even from Vixen.

--- AssetOrganizer/Organizers/MaterialsOrganizer.cs
+++ AssetOrganizer/Organizers/MaterialsOrganizer.cs
@@ -1,8 +1,10 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using VRC.SDK3.Avatars.Components;
 
 public class MaterialsOrganizer : AssetOrganizer.Organizer
 {
@@ -12,27 +14,72 @@ public class MaterialsOrganizer : AssetOrganizer.Organizer
     {
         if (avatar != null)
         {
-            var UsedMats = avatar.GetComponentsInChildren<Renderer>(true).SelectMany(o => o.sharedMaterials).ToArray();
+            var UsedMats = avatar.GetComponentsInChildren<Renderer>(true).SelectMany(o => o.sharedMaterials).ToList();
+
+            var descriptor = avatar.GetComponent<VRCAvatarDescriptor>();
+
+            var Anims = new List<AnimationClip>();
+
+            Anims.AddRange(descriptor.baseAnimationLayers.Where(p => p.animatorController != null).SelectMany(o => o.animatorController.animationClips));
+            Anims.AddRange(descriptor.specialAnimationLayers.Where(p => p.animatorController != null).SelectMany(o => o.animatorController.animationClips));
+
+            foreach (var anim in Anims.Where(anim => anim != null))
+            {
+                var bindings = AnimationUtility.GetObjectReferenceCurveBindings(anim);
+
+                if (bindings != null && bindings.Length > 0)
+                {
+                    foreach (var binding in bindings)
+                    {
+                        foreach (var frame in AnimationUtility.GetObjectReferenceCurve(anim, binding))
+                        {
+                            var obj = frame.value;
+
+                            if (obj is Material mat)
+                            {
+                                if (!UsedMats.Contains(mat))
+                                {
+                                    if (!UsedMats.Contains(mat))
+                                    {
+                                        UsedMats.Add(mat);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             
+            #if VIXEN_IS_PRESENT
+            Debug.Log("Vixen is installed, organizing Vixen Change Properties materials");
+            foreach (var mat in avatar.GetComponentsInChildren<Resilience.Vixen.Components.VixenControl>(true).SelectMany(o => o.subjects).SelectMany(o => o.properties).Select(o => o.materialValue))
+            {
+                if (!UsedMats.Contains(mat))
+                {
+                    UsedMats.Add(mat);
+                }
+            }
+            #endif
+
             path += $"/{ID}";
             CreateDirectory(path);
 
             AssetDatabase.StartAssetEditing();
-            
+
             try
             {
-                for (var index = 0; index < UsedMats.Length; index++)
+                for (var index = 0; index < UsedMats.Count; index++)
                 {
                     var mat = UsedMats[index];
-                    
+
                     if (!AssetDatabase.GetAssetPath(mat).Contains("Assets") || AssetDatabase.GetAssetPath(mat).Contains("/VRCSDK/"))
                     {
                         continue;
                     }
-                    
-                    var progress = (float)index / UsedMats.Length;
-                    EditorUtility.DisplayProgressBar($"Organizing {ID}", $"Processing: {mat.name} ({index + 1}/{UsedMats.Length})", progress);
-                    
+
+                    var progress = (float)index / UsedMats.Count;
+                    EditorUtility.DisplayProgressBar($"Organizing {ID}", $"Processing: {mat.name} ({index + 1}/{UsedMats.Count})", progress);
+
                     var ShaderName = mat.shader.name;
 
                     ShaderName = SanitizeFolderName(ShaderName);
@@ -47,7 +94,7 @@ public class MaterialsOrganizer : AssetOrganizer.Organizer
             {
                 throw;
             }
-            
+
             AssetDatabase.StopAssetEditing();
         }
     }
